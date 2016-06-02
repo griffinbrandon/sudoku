919c205b8afc677fa825f643f77a1dd84afd578b
Mario Tacke <tacke.mario@gmail.com>
2016-06-02 10:34:28 -0700

    (feature) Add console application that reads from a file

---
+++ SudokuCrackerConsole/Cell.cs
@@ -0,0 +1,20 @@
+﻿using Sudoku_Cracker;
+
+namespace SudokuCrackerConsole
+{
+    internal class Cell : ICell
+    {
+        public Cell(int row, int column, int? value = null)
+        {
+            Row = row;
+            Column = column;
+            Value = value;
+        }
+
+        public int Column { get; set; }
+
+        public int Row { get; set; }
+
+        public int? Value { get; set; }
+    }
+}
